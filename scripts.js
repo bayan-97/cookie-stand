baadee09b6b6b587c0da3d8f8c8f0f5df3faf06f
bayan-97 <bayanalalem0@gmail.com>
2020-07-13 23:30:16 +0300

    lab finish7

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 'use strict';
+var tot1=0;
+var tot2=0;
+var workHours = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm'];
+// var places= [];
+// var hours = [];
+
+
 var tot;
-var tot1;
 function Room(min,max,avgCookie,customers,sum,result,header){
   // var res =[];
   var hed=[];
+
   // res.push(result);
   hed.push(header);
   this.header=hed;
@@ -13,6 +20,9 @@ function Room(min,max,avgCookie,customers,sum,result,header){
   this.avgCookie=avgCookie;
   this.sum=sum;
   this.result=result;
+  this.cookeshour=[];
+  // places.push(this);
+
 
 }
 
@@ -37,23 +47,29 @@ Room.prototype.follw2= function () {
   var parentElement = document.getElementById('cookielist');
   var article = document.createElement('article');
   parentElement.appendChild(article);
-  var tot=0;
+  var tot;
   var table1 = document.createElement('table');
   table1.setAttribute('border', '1');
   var tr1 = document.createElement('tr');
   var td1=  document.createElement('td');
   tr1.append(td1);
   td1.textContent = this.header;
-  for (let index1 =1; index1 < 15; index1++) {
+  for (let index1 =0; index1 < workHours.length; index1++) {
+
     var td2 = document.createElement('td');
     this.randomNUM(),
     tr1.append(td2);
     td2.textContent =this.cookieC();
-    tot= tot + this.cookieC();
+    tot= this.cookieC();
+
+    this.sum.push(tot);
 
 
   }
-  
+
+
+
+
   var td3 = document.createElement('td');
   tr1.append(td3);
   td3.textContent =tot;
@@ -66,12 +82,12 @@ Room.prototype.randomNUM= function () {
   this.customers= generateRandomcustomer(this. max,this.min);
 
 };
-var Seattle= new Room(23,65,6.3,10,0,[],'Seattle');
-var Tokyo= new Room(3,24,1.2,10,0,[],'Tokyo');
-var paris= new Room(20,38,2.3,10,0,[] ,'paris');
-var Dubai= new Room(11,38,3.7,10,0,[],'Dubai');
-var lima= new Room(2,16,4.6,10,0,[] ,'lima');
-
+var Seattle= new Room(23,65,6.3,10,[],[],'Seattle');
+var Tokyo= new Room(3,24,1.2,10,[],[],'Tokyo');
+var paris= new Room(20,38,2.3,10,[],[] ,'paris');
+var Dubai= new Room(11,38,3.7,10,[],[],'Dubai');
+var lima= new Room(2,16,4.6,10,[],[] ,'lima');
+var placescook=[Seattle,Tokyo,paris,Dubai,lima];
 
 
 headerdetail();
@@ -80,6 +96,7 @@ Seattle.cookieC();
 Seattle.result.push(Seattle.cookieC());
 Seattle.follw();
 Seattle.follw2();
+
 
 Tokyo.randomNUM(),
 Tokyo.cookieC(),
@@ -96,8 +113,9 @@ Dubai.follw2();
 paris.randomNUM(),
 paris.cookieC(),
 paris.result.push(paris.cookieC());
-paris.follw2();
 paris.follw();
+paris.follw2();
+
 
 lima.randomNUM(),
 lima.cookieC(),
@@ -106,7 +124,7 @@ lima.follw();
 lima.follw2();
 
 
-// footertot(lima.follw2());
+footertot();
 function generateRandomcustomer(max1, min1){
   var generatedCustomer = Math.floor((Math.random()*(max1-min1))+min1);
   return generatedCustomer;
@@ -120,16 +138,16 @@ function headerdetail() {
   var th1  = document.createElement('th');
   tr.append(th1);
   th1.textContent = 0;
-  for (let index1 = 6; index1 < 20; index1++) {
+  for (let index1 = 0; index1 < workHours.length; index1++) {
     var th2 = document.createElement('th');
     tr.append(th2);
-    th2.textContent = index1+' am ';
+    th2.textContent = workHours[index1];
 
 
   }
   var th3 = document.createElement('th');
   tr.append(th3);
-  th3.textContent = 'totle';
+  th3.textContent = 'Daily Location Total';
   table.appendChild(tr);
   article.appendChild(table);
 
@@ -140,20 +158,24 @@ function footertot() {
   var article = document.createElement('article');
   parentElement.appendChild(article);
   var table3 = document.createElement('table');
+  table3.setAttribute('border', '1');
   var tr6 = document.createElement('tr');
   var td6  = document.createElement('td');
   tr6.append(td6);
   td6.textContent ='totle' ;
-  for (let index1 =1; index1 < 15; index1++) {
-    var td7  = document.createElement('td');
-    td7.textContent =totr;
+  for (let index1 =0; index1 < workHours.length; index1++) {
+    for (let index = 0; index <placescook.length; index++) {
+      tot1=tot1+ placescook[index].sum[index1];
+    }
+    var td7 = document.createElement('td');
     tr6.append(td7);
-  
+    td7.textContent = tot1;
+    tot2=tot2+tot1;
   }
 
   var td8  = document.createElement('td');
   tr6.append(td8);
-  td8.textContent =tot1 ;
+  td8.textContent =tot2 ;
   table3.appendChild(tr6);
   article.appendChild(table3);
 
